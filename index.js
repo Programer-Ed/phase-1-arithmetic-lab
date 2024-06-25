c856a5c68a4e1a10b78a6094cfa88f6822e4679a
Edwin Ng'ang'a <moyiedwin8@gmail.com>
2024-06-25 12:39:25 +0300

    adds variables

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply=num1*num2;
+let random = Math.ceil(Math.random() * 10) + 1;
+let num3 = 24;
+let num4 = 5;
+const mod=num3 % num4;
+let max=Math.max(15,16,17,18,19,20);
